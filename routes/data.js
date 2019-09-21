122f85358f46f2cfaa74febbeb92e29349608e71
Julie Chiang <julie.chiang@langlive.com>
2019-09-21 23:18:31 +0800

    basic request

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request-promise");
 
 router.get("/", async (req, res) => {
-  const data = { result: [1] };
-  res.json(data);
+  try {
+    const uri =
+      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+    const options = {
+      uri,
+      method: "GET",
+      json: true
+    };
+    const result = await request(options);
+    const data = { result };
+    res.json(data);
+  } catch (error) {
+    res.status(500);
+  }
 });
 
 module.exports = router;
